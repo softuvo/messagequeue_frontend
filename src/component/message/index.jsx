92a088a1b1c66e8ac17bd11691b2de28bdc00ad5
prabhjot-softuvo <prabhjot.singh@softuvo.com>
2021-05-05 12:53:54 +0530

    implemented message box layout

--- src/component/message/index.jsx
+++ src/component/message/index.jsx
@@ -3,15 +3,18 @@ import { Form, Input, Button, Checkbox, Row, Col, message } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 // API
 import { getUserPost, sendUserPost } from '../../api/Post';
-import { header } from '../../api/apiActions/apiAction'
+import { header } from '../../api/apiActions/apiAction';
+// CSS
+import './message.css';
 
 function Message() {
     const dispatch = useDispatch();
     const loginUserInfo = useSelector(state => state.user.loginUserInfo);
     const loginUserToken = useSelector(state => state.user.loginUserToken);
     console.log('loginUserInfo', loginUserInfo, 'loginUserToken', loginUserToken);
-    const [message, setMessageList] = useState([])
+    const [message, setMessageList] = useState([]);
     const [loader, setLoader] = useState(false);
+    const [textMessage, setTextMessage] = useState('');
     const [form] = Form.useForm();
 
 
@@ -21,8 +24,6 @@ function Message() {
         }
 
     }, [loginUserToken]);
-
-
 
     function getMessageListApi() {
         let data = {
@@ -34,13 +35,10 @@ function Message() {
             setMessageList(res.product);
             setLoader(false);
             form.initialValues();
-
         }).catch(err => {
             setLoader(false);
-
         })
     }
-
 
     const onFinish = (values) => {
         console.log('Received values of form: ', values);
@@ -48,34 +46,44 @@ function Message() {
         sendMessage(values);
     };
 
+    const handleInput = (event) => {
+        setTextMessage(event.target.value);
+    }
+
 
     function sendMessage(values) {
         let data = {
             data: {
-                userId: loginUserInfo._id, ...values
+                userId: loginUserInfo._id, message:textMessage
             }
         };
-        sendUserPost(data,header).then(res => {
+        sendUserPost(data, header).then(res => {
+            setTextMessage('');
             getMessageListApi();
         }).catch(err => {
             setLoader(false);
-
         })
     }
 
     return (
         <React.Fragment>
             <Row>
-                <Col span={12} offset={6}>
-                    {message.length ? message.map((value, index) => {
-                        return (
-                            <React.Fragment key={value.author}>
-                                <div key={index}>
-                                <span >{value.body}</span>
-                                </div>
-                            </React.Fragment>
-                        )
-                    }) : ''}
+                <Col span={6} offset={10} >
+                    <div className="send-message-area">
+                        <div className="scroller">
+                            <div className="message-box">
+                            {message.length ? message.map((value, index) => {
+                                return (
+                                    <React.Fragment key={`${value.author}-${index}`}>
+                                        <div key={index} >
+                                            <span>{value.body}</span>
+                                        </div>
+                                    </React.Fragment>
+                                )
+                            }) : ''}
+                            </div>
+                        </div>
+                    </div>
                     <Form
                         name="normal_login"
                         className="login-form"
@@ -84,16 +92,8 @@ function Message() {
                         }}
                         onFinish={onFinish}
                     >
-                        <Form.Item
-                            name="message"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your message!',
-                                },
-                            ]}
-                        >
-                            <Input.TextArea />
+                        <Form.Item>
+                            <Input.TextArea value={textMessage} onChange={handleInput} required={textMessage ? false : true} />
                         </Form.Item>
                         <Form.Item>
                             <Button type="primary" htmlType="submit" className="login-form-button" loading={loader}>Send Message</Button>
